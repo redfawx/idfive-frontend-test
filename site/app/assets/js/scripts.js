86150861635d9be66aa6148bc3b23bc4d3a811c3
Stephen Toms <steve@Stephens-MacBook-Pro.local>
2019-07-31 07:43:41 -0400

    built animation and functionailty for menu

--- site/app/assets/js/scripts.js
+++ site/app/assets/js/scripts.js
@@ -6,11 +6,31 @@
  * @version 1.0.0
  * Copyright 2019.  licensed.
  */
-(function ($, window, document, undefined) {
 
-  'use strict';
+'use strict';
 
-  console.log('hi');
+var canToggle = true;
+  function toggleMenu() {
+
+      var menuContainer = document.getElementById('menu');
+      var menuPanel = document.getElementById('menucontent');
 
 
-})( window, document);
+      if (canToggle) {
+        menuContainer.style.display = 'block';
+        menuContainer.style.opacity = '1';
+        menuPanel.style.right = '0px';
+        canToggle = false;
+      } else {
+        menuContainer.style.display = 'none';
+        menuContainer.style.opacity = '0';
+        menuPanel.style.right = '-100%';
+        canToggle = true;
+      }
+  }
+
+var menuBtn = document.getElementById('menu-btn');
+var menuCloseBtn = document.getElementById('menu-close-btn');
+
+menuBtn.addEventListener('click', toggleMenu);
+menuCloseBtn.addEventListener('click', toggleMenu);
